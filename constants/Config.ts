a52d50e3f68ee36cfce0bd9660a3f6a22bc9dbb8
Huzaifa <shuzaifa222@gmail.com>
2025-11-26 12:49:11 +0500

    improv: commetns removed

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,27 +1,16 @@
 import * as Device from "expo-device";
 import { Platform } from "react-native";
 
-// Get the API base URL based on the environment
-// For Android emulator, use 10.0.2.2 (special IP to access host machine)
-// For physical devices, use your computer's local IP address
-// For iOS simulator, use localhost
 const getApiBaseUrl = (): string => {
-  // Check if running on Android emulator
   if (Platform.OS === "android" && !Device.isDevice) {
-    // Android emulator uses 10.0.2.2 to access host machine's localhost
     return "http://10.0.2.2:5000/api";
   }
 
-  // For iOS simulator, use localhost
   if (Platform.OS === "ios" && !Device.isDevice) {
     return "http://localhost:5000/api";
   }
 
-  // For physical devices, use your computer's local network IP
-  // Update this IP address to match your current network IP
-  // You can find it by running: ipconfig (Windows) or ifconfig (Mac/Linux)
   return "http://192.168.18.183:5000/api";
 };
 
 export const API_BASE_URL = getApiBaseUrl();
-
